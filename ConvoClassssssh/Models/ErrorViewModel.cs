5636e2e105014d36359f3498c79880a3f866c4dd
mcjos <mcjos@LENOVA>
2024-01-26 14:12:01 -0500

    Add project files.

---
+++ ConvoClassssssh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConvoClassssssh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
